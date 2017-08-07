db5a2003dcb4e3611b538d9ac302bd042d806f16
Michael Owens <mowens@ucdavis.edu>
2017-08-06 19:14:19 -0700

    added 1.3 URlify

--- arrays.js
+++ arrays.js
@@ -32,3 +32,25 @@ let isAnagram = (str1, str2) => {
     return true;
   }
 }
+
+// 1.3 URLify: Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at the end to hold the additional characters, and that you are given the 'true' length of the string.
+/**
+Example:
+Input: "Mr John Smith     ", 13
+Output: "Mr%20John%20Smith"
+*/
+let URLify = (str) => {
+  let input = str.split(' ')
+  let output = [];
+  for(indx in input) {
+    if(input[indx] != '') {
+      output.push(input[indx]);
+    }
+  }
+  return output.join('%20');
+}
+
+// 1.4 Palindrome Permutation: Given a string, write a function to check if it is a permutation of a palindrome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.
+let isPalindromePermutation = (str) => {
+  
+}
