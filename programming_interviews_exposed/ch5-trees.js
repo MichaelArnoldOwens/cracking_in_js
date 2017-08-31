176983b32df047c5006ca3ae39a0209d148d9efc
Michael Owens <mowens@ucdavis.edu>
2017-08-30 18:49:08 -0700

    DFS, treeheight, lowestCommonAncestor

--- programming_interviews_exposed/ch5-trees.js
+++ programming_interviews_exposed/ch5-trees.js
@@ -33,3 +33,62 @@ let BFS = (root, val) => {
   }
   return null;
 }
+
+let DFS = (root, val) => {
+  if(root === null) {
+    return null;
+  }
+  if(root.val === val) {
+    return root;
+  }
+  DFS(root.left, val);
+  DFS(root.right, val);
+}
+
+let preOrder = (root) => {
+  if(root === null) {
+    return null;
+  }
+  console.log(root.val);
+  preOrder(root.left);
+  preOrder(root.right);
+}
+
+let inOrder = (root) => {
+  if(root === null) {
+    return null;
+  }
+  inOrder(root.left);
+  console.log(root.val);
+  inOrder(root.right);
+}
+
+let postOrder = (root) => {
+  if(root === null) {
+    return null;
+  }
+  postOrder(root.left);
+  postOrder(root.right);
+  console.log(root.val);
+}
+
+let getTreeHeight = (root) => {
+  if(root === null) {
+    return null;
+  }
+  return 1 + Math.max(getTreeHeight(root.left), getTreeHeight(root.right));
+}
+
+let findLowestCommonAncestor = (root, val1, val2) {
+  const val = root.val;
+  while(root != null) {
+    if(val > val1 && val > val2) {
+      root = root.left;
+    } else if(val < val1 && val < val2) {
+      root = root.right;
+    } else {
+      return root;
+    }
+  }
+  return null;
+}
