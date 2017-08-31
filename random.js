17826ca4bb2f9e51aabb647938bade3ffc8637c0
Michael Owens <mowens@ucdavis.edu>
2017-08-30 18:49:21 -0700

    executeAll, flatten

--- random.js
+++ random.js
@@ -1,4 +1,5 @@
 /**
+ * TODO: FIX
  * Given a directed graph, find the shortest path between 2 nodes if one exists
  */
 
@@ -31,3 +32,61 @@ let shortestPath = (start, end) => {
   }
   return null;
 }
+
+
+
+var func1 = function(cb) { // Make AJAX call.
+  setTimeout(function() { // Wait for the internet.
+    cb({name: 'Bob'}); // Return response.
+  }, 500);
+};
+
+var func2 = function(cb) {
+  setTimeout(function() {
+    cb({name: 'Jim'});
+  }, 200);
+};
+
+var func3 = function(cb) {
+  setTimeout(function() {
+    cb({name: 'Eli'});
+  }, 100);
+};
+
+
+function executeAll(fcnArr, cb) {
+    let result = [];
+    let counter = 0;
+        
+    let newCb = (i) => { // I need to know the index here.
+        return (arg) => {
+            counter++;
+            result[i] = arg;
+            if(counter === fcnArr.length) { 
+                cb(result);
+            }
+        }
+    }
+    
+    for(let i = 0; i < fcnArr.length; i++) {
+
+      fcnArr[i](newCb(i)) // I know the index.
+
+    }
+}
+
+executeAll([func1, func2, func3], function(result) {console.log(result)});
+// [ { name: 'Bob' }, { name: 'Jim' }, { name: 'Eli' } ]
+
+let flatten = (arr) => {
+  let result = [];
+  for(let i = 0; i < arr.length; i++) {
+    if(Array.isArray(arr[i])) {
+      result = [...result, ...flatten(arr[i])];
+    } else {
+      result = [...result, arr[i]];
+    }
+  }
+  return result;
+}
+
