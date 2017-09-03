df4c10cf0a1d600827d7c97af2c0cde667719085
Michael Owens <mowens@ucdavis.edu>
2017-09-02 20:44:32 -0700

    added insertSort

--- random.js
+++ random.js
@@ -90,3 +90,79 @@ let flatten = (arr) => {
   return result;
 }
 
+
+
+
+
+
+function parseQueryString(query) {
+  let parseQuery = query.substring(1).split('&');
+  console.log(parseQuery)
+  let json = {};
+  for(let i in parseQuery) {
+      let splitParams = parseQuery[i].split('=');
+      // [key, value] || [key]
+      if(splitParams.length === 2) {
+          checkExistingProp(json, splitParams)
+      } else {
+          if(json.hasOwnProperty(splitParams[0])) {
+              console.log('splitParams in single: ', splitParams)
+              let arr = [];
+              if(Array.isArray(json[splitParams[0]])) {
+                  arr.push(json[splitParam[0]])
+              }
+//                 arr = [json[splitParams[0]]];
+              json[splitParams[0]] = [...arr, true]
+          } else {
+              json[splitParams[0]] = true;                
+          }
+          
+      }
+      
+  }
+  
+  return json;
+}
+
+// you dropped out of the hangouts call. Should just be a few more min
+// you dropped out of the hangouts call. Should just be a few more min
+// you dropped out of the hangouts call. Should just be a few more min
+// you dropped out of the hangouts call. Should just be a few more min
+
+// Array.isArray(...)
+
+let checkExistingProp = (json, splitParams) => {
+  console.log('splitParams in double: ', splitParams)
+  if(json.hasOwnProperty(splitParams[0])) {
+      let arr = [];
+      if(Array.isArray(json[splitParams[0]])) {
+          arr.push(json[splitParams[0]])   
+      }
+//         arr = [json[splitParams[0]]];
+      console.log('arr: ', arr)
+      json[splitParams[0]] = [...arr, splitParams[1]]
+   } else {
+      json[splitParams[0]] = splitParams[1]; 
+  }
+  return json;
+}
+
+// -> {foo: "hello", bar: "world", baz: [true, "again"]}
+console.log(parseQueryString('?foo=hello&baz&bar=world&baz=again&baz=3'));
+
+let insertSort = (arr) => {
+  let result = [arr[0]];
+  for(let i = 1; i < arr.length; i++) {
+    let curr = arr[i];
+    for(let k = 0; k < result.length; k++) {
+      if(curr < result[k]) {
+        result.splice(k, 0, curr);
+        break;
+      }
+    }
+  }
+  return result;
+}
+
+let arr2 = [20, 2, 4, 3, 7, 21, 2, 1];
+console.log(insertSort(arr2))
