ff05ce64433190d99df5990393955d52f3227fbc
Michael Owens <mowens@ucdavis.edu>
2019-01-05 15:16:49 -0800

    sots

---
+++ sorts.js
@@ -0,0 +1,33 @@
+// import assert from 'assert';
+const assert = require('assert');
+
+
+// Insertion Sort
+const insertionSort = function (arr) {
+  return arr;
+}
+
+// describe('Insertion Sort', function () {
+//   it('should sort', function () {
+//     let arr = [14, 33, 27, 10, 35, 19, 42, 44];
+//     const result = [10, 14, 19, 27, 33, 35, 42, 44];
+//     assert.equal(result, insertionSort(arr));
+//   });
+// });
+
+// random stuff to be moved
+
+function foo(x) {
+  if(x === 0) return x;
+  console.log(x)
+  return foo(x - 1);
+}
+var yoo = foo(5);
+console.log(yoo);
+
+// describe('random', function() {
+//   it('should do stuff', function() {
+
+//   });
+// })
+
